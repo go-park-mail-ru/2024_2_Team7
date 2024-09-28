71614c2816640e78259570b0402d95ba17752ed4
Ko71k <33371176+Ko71k@users.noreply.github.com>
2024-09-28 23:58:34 +0300

    Update middleware.go
    127-->localhost

--- internal/auth/middleware.go
+++ internal/auth/middleware.go
@@ -34,7 +34,7 @@ func (h *Handler) GetEventsHandler(w http.ResponseWriter, r *http.Request) {
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Устанавливаем CORS-заголовки
-		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5500")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
