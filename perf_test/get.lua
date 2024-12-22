9a0a68170eca888b8f90d5684fa57a2d8f43d20d
Alexander Rudenko <ram181524@gmail.com>
2024-12-22 22:48:22 +0300

    tests_3

--- perf_test/get.lua
+++ perf_test/get.lua
@@ -1,11 +1,5 @@
-local counter = 0
-local max_requests = 100000
-
+math.randomseed(os.time())
 request = function()
-    if counter >= max_requests then
-        wrk.thread:stop()  -- Остановка при достижении 100,000 запросов
-    end
-    counter = counter + 1
     local id = math.random(1, 100000)
     local path = "/events/" .. id
     return wrk.format("GET", path)
